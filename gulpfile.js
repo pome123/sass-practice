0cd19e93552c41594708e9ff43de5e08b69d44ec
fujine <fujine@fujinenoMacBook-Pro.local>
2019-10-03 09:38:36 +0900

    watch機能を使ってSassファイルを自動コンパイルできるようにした

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,27 @@ const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
-// style.scssのタスクを作成する
+// style.scssの監視タスクを作成する
 gulp.task("default", function() {
-    // style.scssファイルを取得
-    return (
-        gulp
-            .src("css/style.scss")
-            // Sassのコンパイルを実行
-            .pipe(
-                sass({
-                    outputStyle: "expanded"
-                })
-            )
-            // cssフォルダーは以下に保存
-            .pipe(gulp.dest("css"))
-    );
-});
+    // style.scssのファイルを監視
+    return gulp.watch("css/style.scss", function() {
+    // style.scssの更新があった場合の処理
 
+        // style.scssファイルを取得
+        return (
+            gulp
+                .src("css/style.scss")
+                // Sassのコンパイルを実行
+                .pipe(
+                    sass({
+                        outputStyle: "expanded"
+                    })
+                    // Sassのコンパイルエラーを実行
+                    // (これがないと自動的に止まってしまう)
+                    .on("error", sass.logError)
+                    )
+                // cssフォルダーは以下に保存
+                .pipe(gulp.dest("css"))
+        );
+    });
+});
